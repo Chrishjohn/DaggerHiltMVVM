4ba595edb436e3051cf7c9a2272a63e01509a18c
Chrishjohn <ranakaransinh13@gmail.com>
2022-07-20 23:11:19 +0530

    commit

--- app/src/main/java/com/example/hiltmvvm/HiltApplication.kt
+++ app/src/main/java/com/example/hiltmvvm/HiltApplication.kt
@@ -3,6 +3,7 @@ package com.example.hiltmvvm
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
 
+//application
 @HiltAndroidApp
 class HiltApplication: Application() {
     override fun onCreate() {
